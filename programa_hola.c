df4b39d4b344326575bbe9874dc6e47958282af9
antoniosanch <tonisanch36@gmail.com>
2018-08-13 09:48:02 -0500

    resolviendo conflicto

--- programa_hola.c
+++
@@ -1,7 +0,0 @@
-/* Este es un comentario me sirve para escribir informacion relevante. Este programa imprime la tipica frase "hola mundo" en la pantalla. Creado por Antonio Sanchez.*/
-#include <stdio.h>
-int main ()
-{
-printf("Hola Toño \n");
-printf("Este es el primer programa del curso \n");
-}

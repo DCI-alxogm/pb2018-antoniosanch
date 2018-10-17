968b1c16e431e540fca7afde7c20d317775432ae
antoniosanch <tonisanch36@gmail.com>
2018-10-17 08:54:10 -0500

    miercoles ejemplos

---
+++ semana10/tremp.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+void temp();
+int main (){
+	temp();
+	return 0;
+}
---
+++ semana10/tremp2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void temp(){
+float C,F,K;
+printf("Introduzca su temperatura en celsius a convertir: \n");
+scanf("%f",&C);
+F=(C*1.8)+32;
+K=C+273.15;
+printf("c=%f \t",C);
+printf("f=%f \t",F);
+printf("k=%f \n",K);
+}
